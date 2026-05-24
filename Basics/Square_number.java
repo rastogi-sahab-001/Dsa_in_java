cf197b5ee59c9792573718435313975a95bffe92
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-05-24 15:06:00 +0530

    commit

---
+++ Basics/Square_number.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Square_number {
+        public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number:-");
+        int n = sc.nextInt();
+        System.out.println(n * n + " " + "is the square of" + " " + n);
+    }
+}
