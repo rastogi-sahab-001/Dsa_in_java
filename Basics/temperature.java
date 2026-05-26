4179a4d4c0622feb36b2af23f0067ba9bca3a8a2
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-05-26 12:54:44 +0530

    commit

---
+++ Basics/temperature.java
@@ -0,0 +1,12 @@
+// Celsius to Fahrenheit
+import java.util.*;
+public class temperature {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter celsius:-");
+        double celsius = sc.nextDouble();
+        double Fahrenheit = ( 9.0 / 5.0 ) * celsius + 32;
+        System.out.print(Fahrenheit);
+        sc.close();
+    }
+}
