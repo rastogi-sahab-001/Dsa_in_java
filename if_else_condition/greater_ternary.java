67f65514271874d27810e150511707b056b03aac
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-05-31 20:06:12 +0530

    My project

--- if_else_condition/greater_ternary.java
+++ if_else_condition/greater_ternary.java
@@ -9,9 +9,9 @@ public class greater_ternary {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number");
         int a = sc.nextInt();
-        System.out.println("Enter Second Number");
+        System.out.print("Enter Second Number");
         int b = sc.nextInt();
-        System.out.println("Enter Third Number");
+        System.out.print("Enter Third Number");
         int c = sc.nextInt();
         int greatest = (a > b)
                 ? ((a > c) ? a : c)
