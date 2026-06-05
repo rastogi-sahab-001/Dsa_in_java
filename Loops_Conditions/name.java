e6b1e76860c2741d765411e577612dc73a0c71c9
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-06-05 09:24:46 +0530

    commit

---
+++ Loops_Conditions/name.java
@@ -0,0 +1,14 @@
+package Loops_Conditions;
+import java.util.*;
+public class name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name:-");
+        int n = sc.nextInt();
+        for(int i =1; i<=n; i++){
+            System.out.println(i+ " " + "Krishna");
+            sc.close();
+        }
+    }  
+}
+    
