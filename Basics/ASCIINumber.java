dd4c10070f668f0753a6508b260ffe1937d37a51
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-06-08 18:00:00 +0530

    Contribution for June 8

--- Basics/ASCIINumber.java
+++ Basics/ASCIINumber.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class ASCIINumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Value :-");
-        int n = sc.nextInt();
+
         char ch = (char) n;
         System.out.println("ASCII Value is " + ch);
         sc.close();
