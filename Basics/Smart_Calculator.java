226b052df565c6c25281313a247ac16d6141428f
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-06-08 18:00:00 +0530

    Contribution for June 8

--- Basics/Smart_Calculator.java
+++ Basics/Smart_Calculator.java
@@ -42,6 +42,7 @@ public class Smart_Calculator {
 
         System.out.println("Modulus: " + (n % x));
 
+        double avg = (n + x) / 2.0;
         System.out.println("Average: " + avg);
 
         // Even or Odd
