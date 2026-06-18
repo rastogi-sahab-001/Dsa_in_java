495aae6b1dacdfe70acf7fa3ff53dbc0baa20e2e
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-06-18 20:05:15 +0530

    commit

--- Basics/temperature.java
+++ Basics/temperature.java
@@ -5,7 +5,7 @@ public class temperature {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter celsius:-");
         double celsius = sc.nextDouble();
-
+        double Fahrenheit = ( 9.0 / 5.0 ) * celsius + 32;
         System.out.print(Fahrenheit);
         sc.close();
     }
