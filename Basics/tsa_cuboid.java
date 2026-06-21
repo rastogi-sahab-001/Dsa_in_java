8744c74bc99976d064f81301a79bc36c5c0cf539
rastogi-sahab-001 <krishna.rastogi_cs24@gla.ac.in>
2026-06-21 20:29:39 +0530

    commit

--- Basics/tsa_cuboid.java
+++ Basics/tsa_cuboid.java
@@ -1,7 +1,7 @@
 public class tsa_cuboid {
     public static void main(String[]args){
         int l=10, b=10, h=10;
-        int cuboid = 2*((l*b)+(b*h)+(h*l));
+
         System.out.println(cuboid);
     }
 }
